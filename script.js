4debe8c54e4593632e38b20cac1eb3692768e6f5
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-04 16:20:09 +0900

    Removed most of the functions
    Remove all but two functions. Left the getComputerSelection
     and generateRandomNum functions to get the computers random selection
     between rock, paper, and scissors.

--- script.js
+++ script.js
@@ -1,108 +1,47 @@
-function playGame()
-{    
-    //Declares the variables to count how many rounds the user and computer wins
-    var user = 0
-    var comp = 0
+const btnRock = document.querySelector('.btn-rock');
+const btnPaper = document.querySelector('.btn-paper');
+const btnScissors = document.querySelector('.btn-scissors');
+const counter = document.querySelectorAll('.image-btn');
 
-    //Loops 5 times
-    for (let i=0; i<5; i++)
-    {
-
-        var userSelection = getUserSelection()
-        var computerSelection = getComputerSelection()
-
-        var point = singleRound(userSelection, computerSelection)
-        
-        //Count the rounds the user and computer wins each round
-        switch (point)
-        {
-            case "userWins":
-                user += 1
-                break
-            case "computerWins":
-                comp += 1
-                break
-            case "draw":
-                break
-                
-        }
-    }
-
-    //Determines the winner by comparing how many rounds the computer and user wins
-    if (user > comp)
-    {
-        return "You win the game!  " + user + "/5"
-    }
-    else if (user < comp)
-    {
-        return "You win the game!  " + comp + "/5"
-    }
-}
-
-function getUserSelection()
+btnRock.addEventListener('click', () =>
 {
-    var userAns = prompt("Rock, Paper, Scissors, Shoot: ").toLowerCase()
-    //Returns users input only when its equal to rock, paper or scissors, otherwise,it will return an error message and loop the function
-    if (userAns === "rock" || userAns === 'paper' || userAns === "scissors")
-    {
-        return userAns
-    }
-    else
-    {
-        alert("Selection doesn't equal Rock, Paper, or Scissors! Try Again!");
-        getUserSelection();
-    }
-}
+    playRound('Rock');
+});
+
+btnPaper.addEventListener('click', () =>
+{
+    playRound('Paper');
+});
+
+btnScissors.addEventListener('click', () =>
+{
+    playRound('Scissors');
+});
 
 function generateRandomNum(min, max)
 {
     //Generates a random number between 1 and 3
     return Math.floor(Math.random() * (max - min +1) + min);
-}
+};
 
 function getComputerSelection()
 {
-    let min = 1
-    let max = 3
+    let min = 1;
+    let max = 3;
     //Calls the random number function to get a number between 1 and 3
-    var randomNum = generateRandomNum(min, max)
+    var randomNum = generateRandomNum(min, max);
     
     //Returns rock, paper, or scissors based on the random number generated
     switch(randomNum)
     {
         case 1:
-            return "rock"
-            break
+            return "Rock";
+            break;
         case 2:
-            return "paper"
-            break
+            return "Paper";
+            break;
         case 3:
-            return "scissors"
-            break
+            return "Scissors";
+            break;
     }
-}
-
-function singleRound(userSelection, computerSelection)
-{
-    //Checks if the user wins, if they do, shows victory message and tells the play game function to give the user a point
-    if ((userSelection === "rock" && computerSelection === "scissors") || (userSelection === "scissors" && computerSelection === "paper") || (userSelection === "paper" && computerSelection === "rock"))
-    {
-        alert(userSelection.charAt(0).toUpperCase() + userSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)  + "! You win!");
-        return "userWins"
-    }
-    //Checks if the computer wins, if they do, shows defeat message and tells the play game function to give the computer a point
-    else if ((computerSelection === "rock" && userSelection === "scissors") || (computerSelection === "scissors" && userSelection === "paper") || (computerSelection === "paper" && userSelection === "rock"))
-    {
-        alert(computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)  + " beats " + userSelection.charAt(0).toUpperCase() + userSelection.slice(1)  + "! You lose!");
-        return 'computerWins'
-    }
-    //Checks if the users and computer selections are the same, and tll the play game function to give no points
-    else if (userSelection === computerSelection)
-    {
-        alert(computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)  + " equals " + userSelection.charAt(0).toUpperCase() + userSelection.slice(1)  + "! It's a draw!");
-        return "draw"
-    }
-}
-
-var winner = playGame()
-alert(winner)
+};
